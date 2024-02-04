f468cbf6e276faad8a900dfdb86774b56ef9a572
Luis Angel <angelfgdeveloper@gmail.com>
2024-02-04 14:27:07 -0600

    Creacion de consola con banderas para imprimir tabla de multiplicacion con yargs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 
 // argumentos validos
-console.log(process.argv);
+// console.log(process.argv);
+// const [ tsnode, app, ...args] = process.argv;
+// console.log(args);
 
-const [ tsnode, app, ...args] = process.argv;
+import { yarg } from './config/plugins/args.plugin';
+// console.log(process.argv);
+// console.log(yarg);
+// console.log(yarg.b);
 
-console.log(args);
+// Funcion anonima auto invocada
+(async() => {
+  // console.log('Ejecutando');
+  await main();
+})();
+
+async function main() {
+  // console.log('Main ejecutado');
+
+  console.log(yarg);
+
+}
