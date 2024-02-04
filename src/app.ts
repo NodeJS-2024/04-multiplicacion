8b0aadc7d4282cbefa9207c46e715b47f0dfcf66
Luis Angel <angelfgdeveloper@gmail.com>
2024-02-04 13:08:01 -0600

    Generando tarea de mulitplicacion con creacion de carpeta

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
+import fs from 'fs';
 
-const message: string = 'Hola Mundo!';
+let outputMessage = '';
+const base = 5;
+const headerMessage = `
+===========================
+       Tabla del ${ base }
+===========================\n
+`;
 
-console.log({message});
+for (let i = 1; i <= 10; i++) {
+  outputMessage += `${ base } x ${ i } = ${ base * i }\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage);
+
+const outputPath = 'outputs';
+
+// Crear la carpeta de forma recursiva
+// ejemplo de outputs/folder1/folder2 ...
+fs.mkdirSync(outputPath, { recursive: true });
+
+// Escribe en el archivo el texto y lo crea
+fs.writeFileSync(`${ outputPath }/tabla-${ base }.txt`, outputMessage);
+console.log('File created!');
+
+
 
