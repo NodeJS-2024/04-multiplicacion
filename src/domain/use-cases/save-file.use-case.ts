f5dfa5725101470a88ccba80e946cab6cdc3e26e
Luis Angel <angelfgdeveloper@gmail.com>
2024-02-04 15:21:32 -0600

    Caso de uso para la creacion de archivos en memoria

---
+++ src/domain/use-cases/save-file.use-case.ts
@@ -0,0 +1,41 @@
+import fs from 'fs';
+
+export interface SaveFileUseCase {
+  execute: (options: Options) => boolean;
+}
+
+export interface Options {
+  fileContent: string;
+  destination?: string;
+  fileName?: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+
+  constructor() {
+    // repository: StorageRespository
+  }
+
+  execute({
+    fileContent, 
+    destination: fileDestination = 'outputs', 
+    fileName = 'table'
+  }: Options): boolean {
+
+    try {
+      // Crear la carpeta de forma recursiva
+      // ejemplo de outputs/folder1/folder2 ...
+      fs.mkdirSync(fileDestination, { recursive: true });
+  
+      // Escribe en el archivo el texto y lo crea
+      fs.writeFileSync(`${ fileDestination }/${ fileName }.txt`, fileContent);
+      
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+
+  }
+
+}
